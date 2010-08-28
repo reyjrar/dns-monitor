dc10187e4713500eea2dedec249035e4c8671bae
Brad Lhotsky <brad.lhotsky@gmail.com>
2010-08-27 23:06:01 -0400

    client::stats module logging data

--- lib/POE/Component/dns/monitor/sniffer/plugin/packet/logger.pm
+++ lib/POE/Component/dns/monitor/sniffer/plugin/packet/logger.pm
@@ -2,7 +2,51 @@ package POE::Component::dns::monitor::sniffer::plugin::packet::logger;
 
 use strict;
 use warnings;
+use POE;
+use DateTime;
+use YAML;
 
+sub spawn {
+	my $self = shift;
+	my %args = @_;
+
+	die "Bad Config" if ref $args{Config} ne 'HASH';
+	die "Bad Model" unless ref $args{DBICSchema};
+	die "No Alias" unless length $args{Alias};
+
+	my $sess = POE::Session->create( inline_states => {
+		_start	=> sub { $poe_kernel->yield( 'packet_logger_start', \%args ); },
+		_stop 	=> sub { },
+		packet_logger_start => \&packet_logger_start,
+		process => \&process,
+	});
+
+	return $sess->ID;
+}
+
+sub server_stats_start {
+	my($kernel,$heap,$args) = @_[KERNEL,HEAP,ARG0];
+
+	$kernel->alias_set( $args->{Alias} );
+	
+	# Store stuff in the heap
+	$heap->{log} = $args->{LogSID};
+	$heap->{model} = $args->{DBICSchema};
+}
+
+sub process {
+	my ( $kernel,$heap,$dnsp,$srv,$cli ) = @_[KERNEL,HEAP,ARG0,ARG1,ARG2];
+
+	#my $stats = $heap->{model}->resultset('server::stats')->find_or_create(
+	#	{
+	#		server_id	=> $srv->id,
+	#		day 		=> $dt->ymd,
+	#	}
+	#);	
+
+	# Check for query/response
+	if( $dnsp->header->qr ) { }
+}
 
 1;
 
