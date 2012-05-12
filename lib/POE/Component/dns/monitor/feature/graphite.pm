2a3713b298ae465aca4f89e53ab48e2eca1dbb77
Brad Lhotsky <brad.lhotsky@gmail.com>
2012-05-12 16:38:15 +0200

    make graphite feature API compliant for feature load

--- lib/POE/Component/dns/monitor/feature/graphite.pm
+++ lib/POE/Component/dns/monitor/feature/graphite.pm
@@ -2,4 +2,12 @@ package POE::Component::dns::monitor::feature::graphite;
 
 use POE;
 
+# All features have to tell me what they provide!
+sub provides { return 'stats'; }
+
+sub spawn {
+    return $$;
+}
+
+
 1;
